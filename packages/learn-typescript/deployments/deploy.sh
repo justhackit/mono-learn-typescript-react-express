da7e194dfdf8b66b7e19122c185892e3322fc1ec
justhackit <aj.nextstep@gmail.com>
2023-06-09 11:42:11 -0500

    just save it

--- packages/learn-typescript/deployments/deploy.sh
+++ packages/learn-typescript/deployments/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
-kubectl delete -f learn-typescript-deploy.yaml
-kubectl apply -f learn-typescript-deploy.yaml
+kubectl delete -f learn-typescript-deploy.yaml -n dev
+kubectl apply -f learn-typescript-deploy.yaml -n dev
